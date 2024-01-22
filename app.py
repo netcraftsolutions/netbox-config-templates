6581ffbad0a42eaacb4552e0d7364adc76cf8773
Vladislav Bartosik <bartosik@netcraft.solutions>
2024-01-22 12:38:19 +0100

    app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+import json
+from jinja2 import Environment, FileSystemLoader
+
+with open('config-contexts/global.json') as f:
+    config_context = json.load(f)
+
+environment =  Environment(loader=FileSystemLoader('./'))
+template = environment.get_template('config-templates/base_switch.j2')
+
+final_config = template.render(config_context)
+
+print(final_config)
+
+
